5970dbe128aeea9f0c3e3480bed85857822db76e
shanice <shanicesmith.work@hotmail.com>
2024-06-23 01:18:50 -0400

    add more mock data and add a future todo

--- js/mock-data.js
+++ js/mock-data.js
@@ -1,4 +1,4 @@
-const mockCareers = [ "Actor", "Writer", "Test Subject", "Doctor", "Musician" ];
+const mockCareers = [ "Actor", "Writer", "Test Subject", "Doctor", "Musician", "Freelancer", "Social Media" ];
 const mockTraits = [ "Shy", "Vain", "Animal Lover", "Evil", "Night Owl", "Grumpy", "Loner", "Neat", "Handy", "Genius" ];
 const mockAspirations = [ "Friend of the Animals", "Bestselling Author", "Big Happy Family", "Master Mixologist", "Nerd Brain", "City Native", "Soulmate", "Outdoor Enthusiast" ];
 
@@ -6,3 +6,5 @@ const mockHairColor = [ "Black", "Brown", "Blonde", "Red", "Green", "Gray"];
 const mockEyesColor = [ "Brown", "Green", "Hazel", "Blue" ];
 const mockAge = [ "Teen", "Young Adult", "Adult", "Elder" ];
 const mockStyle = [ "Basics", "Country", "Hipster", "Rocker", "Boho" ];
+
+// TODO: Create script to scrape web to extract more data from Sims Wikipedia
